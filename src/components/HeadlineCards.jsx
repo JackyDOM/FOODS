2ac5ac5a2df0ae941b22a0f6705bf164a35ef915
JackyDOM <146927846+JackyDOM@users.noreply.github.com>
2023-10-09 10:56:13 +0700

    Add files via upload

---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+
+const HeadlineCards = () => {
+  return (
+    <div className='max-w-[1640px] mx-auto p-4 
+    py-12 grid md:grid-cols-3 gap-6'>
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50
+        rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, 8000's Out</p>
+          <p className='px-2'>Throught 8/26</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' 
+        src="https://images.pexels.com/photos/8471703/pexels-photo-8471703.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50
+        rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>New Restaurants</p>
+          <p className='px-2'>Added Daily</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' 
+        src="https://images.pexels.com/photos/4393021/pexels-photo-4393021.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50
+        rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>We Deliver Dessert Too</p>
+          <p className='px-2'>Tasty Treat</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' 
+        src="https://images.pexels.com/photos/918327/pexels-photo-918327.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50
+        rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Foods Is Your Life</p>
+          <p className='px-2'>Very Yummmy</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' 
+        src="https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50
+        rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Foods Is Your Life</p>
+          <p className='px-2'>Very Yummmy</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' 
+        src="https://images.pexels.com/photos/3186654/pexels-photo-3186654.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50
+        rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Foods Is Your Life</p>
+          <p className='px-2'>Very Yummmy</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' 
+        src="https://images.pexels.com/photos/299347/pexels-photo-299347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+    </div>
+  );
+}
+ 
+
+
+export default HeadlineCards;
